fa0b89c3068f812e07eba295271de15ab786f495
Raúl Galindo <raulg@pikotea.com>
2025-09-16 14:27:28 +0200

    Google id updated

--- src/main/java/galindo/raul/virtualclubs/services/GoogleAuthService.java
+++ src/main/java/galindo/raul/virtualclubs/services/GoogleAuthService.java
@@ -4,6 +4,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +15,14 @@ import java.util.Collections;
 @Service
 public class GoogleAuthService {
 
-    @Value("${google.id}")
-    private String CLIENT_ID;
-
     private final GoogleIdTokenVerifier verifier;
 
-    public GoogleAuthService() throws Exception {
+    @Autowired
+    public GoogleAuthService(
+            @Value("${google.id}") String clientId
+    ) throws Exception {
         verifier = new GoogleIdTokenVerifier.Builder(GoogleNetHttpTransport.newTrustedTransport(), GsonFactory.getDefaultInstance())
-                .setAudience(Collections.singletonList(CLIENT_ID))
+                .setAudience(Collections.singletonList(clientId))
                 .build();
     }
 
